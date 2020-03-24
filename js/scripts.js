876e2efa7000ded4ffe2dcc7bb22f67768d13b7a
austinb847 <austinb847@icloud.com>
2020-03-24 10:41:07 -0700

    add new image and add toggle funcionality to toggle between hiding and showing the image on the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  $(".clickable").click(function(){
+  $("#walrus-hidden .clickable, #walrus-showing .clickable").click(function(){
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
+  });
+  $("#hide-image .clickable, #show-image .clickable").click(function(){
+    $("#show-image").toggle();
+    $("#hide-image").toggle();
   });
 });
